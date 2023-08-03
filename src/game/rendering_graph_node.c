241d31756be2d6689e3bb834ef861e7dad911b92
Gregory Heskett <gheskett@gmail.com>
2023-08-03 15:35:52 -0400

    Fix persona console issues (kinda)

--- src/game/rendering_graph_node.c
+++ src/game/rendering_graph_node.c
@@ -97,6 +97,7 @@ struct RenderModeContainer renderModeTable_1Cycle[2] = {
         [LAYER_TRANSPARENT] = G_RM_AA_XLU_SURF,
         [LAYER_TRANSPARENT_INTER] = G_RM_AA_XLU_SURF,
         [LAYER_FORCE_ALPHA_ON_TOP] = G_RM_AA_TEX_EDGE,
+        [LAYER_FORCE_TRANSPARENT_ON_TOP] = G_RM_AA_XLU_SURF,
     } },
     [RENDER_ZB] = { {
         [LAYER_FORCE] = G_RM_ZB_OPA_SURF,
@@ -115,6 +116,7 @@ struct RenderModeContainer renderModeTable_1Cycle[2] = {
         [LAYER_TRANSPARENT] = G_RM_AA_ZB_XLU_SURF,
         [LAYER_TRANSPARENT_INTER] = G_RM_AA_ZB_XLU_INTER,
         [LAYER_FORCE_ALPHA_ON_TOP] = G_RM_AA_TEX_EDGE,
+        [LAYER_FORCE_TRANSPARENT_ON_TOP] = G_RM_AA_XLU_SURF,
     } } };
 
 /* Rendermode settings for cycle 2 for all 13 layers. */
@@ -136,6 +138,7 @@ struct RenderModeContainer renderModeTable_2Cycle[2] = {
         [LAYER_TRANSPARENT] = G_RM_AA_XLU_SURF2,
         [LAYER_TRANSPARENT_INTER] = G_RM_AA_XLU_SURF2,
         [LAYER_FORCE_ALPHA_ON_TOP] = G_RM_AA_TEX_EDGE2,
+        [LAYER_FORCE_TRANSPARENT_ON_TOP] = G_RM_AA_XLU_SURF,
     } },
     [RENDER_ZB] = { {
         [LAYER_FORCE] = G_RM_ZB_OPA_SURF2,
@@ -154,6 +157,7 @@ struct RenderModeContainer renderModeTable_2Cycle[2] = {
         [LAYER_TRANSPARENT] = G_RM_AA_ZB_XLU_SURF2,
         [LAYER_TRANSPARENT_INTER] = G_RM_AA_ZB_XLU_INTER2,
         [LAYER_FORCE_ALPHA_ON_TOP] = G_RM_AA_TEX_EDGE2,
+        [LAYER_FORCE_TRANSPARENT_ON_TOP] = G_RM_AA_XLU_SURF,
     } } };
 
 ALIGNED16 struct GraphNodeRoot *gCurGraphNodeRoot = NULL;
